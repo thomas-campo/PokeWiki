3560379f280f740ebbad6a3c6af9b1b1677f0a2b
Thomas campo <thomascampocood@gmail.com>
2023-11-10 21:00:37 -0300

    first commit

--- src/public/js/home.js
+++
@@ -1,8 +0,0 @@
-const API_URL = "https://pokeapi.co/api/v2/pokemon/4/";
-
-fetch(API_URL)
-    .then(resp => resp.json())
-    .then(data =>{
-        console.log(data,"data")
-    })
-    .catch(error => console.log(error.message));
--- src/router/views.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import viewsController from "../controller/views.controller.js";
-
-const router = Router();
-
-router.get("/",viewsController.getHome)
-
-export default router;
